899b8beb2c72ba578da99f4b08f6d517c6599f64
villep <pekvil16@gmail.com>
2022-09-20 12:00:14 +0300

    data yhdessa JS oliossa

--- kurssitiedot/src/App.js
+++ kurssitiedot/src/App.js
@@ -1,28 +1,30 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
       <Content 
-        parts = {parts}
+        course = {course}
       />
       <Total
-        parts = {parts}
+        course = {course}
       />
     </div>
   )
@@ -30,9 +32,9 @@ const App = () => {
 const Content = (props) => {
   return(
     <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part name={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+      <Part name={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
+      <Part name={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
     </>
   )
 }
@@ -44,7 +46,7 @@ const Part = (props) =>{
 }
 const Total = (props)  =>{
   return(
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
   )
 }
 const Header = (props) =>{
